4468a14b37176d6f66e9ce8ab6613257978e2ec0
Mattias <Mattias.Folke@gmail.com>
2014-03-23 19:33:03 +0100

    fixed scripts for getting information from db

--- scripts/isConnected.php
+++ scripts/isConnected.php
@@ -1,6 +1,6 @@
 <?php
 
-$link = new mysqli("mysql-vt2013.csc.kth.se:3306/matfol","matfoladmi","FU0B1klD", "matfol");
+$link = new mysqli("mysql-vt2013.csc.kth.se:3306/matfol","matfoladmin","FU0B1klD", "matfol");
 if (!$link) {
 	echo '';
  die('Could not connect: ' . mysql_error());
--- scripts/loadAll.php
+++ scripts/loadAll.php
@@ -6,5 +6,35 @@ if (!$link) {
  die('Could not connect: ' . mysql_error());
 }
 
-echo $_GET['table'];
+    $query="SELECT * FROM $_GET[table]";
+
+    // get data from db
+    $result=$link->query($query) or trigger_error($mysqli->error." [$query]");
+
+$rows = array();
+while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+	$rows[] = $row;
+}
+
+
+// Encode all string children in the array to html entities.
+array_walk_recursive($rows, function(&$item, $key) {
+    if(is_string($item)) {
+        $item = htmlentities($item);
+    }
+});
+$json = json_encode($rows);
+
+// Decode the html entities and end up with unicode again.
+$json = html_entity_decode($json);
+
+echo $json;
+
+
+/* free result set */
+$result->free();
+/* close connection */
+mysqli_close($link);
+
 ?>
+
